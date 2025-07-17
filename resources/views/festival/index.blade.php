c1bec57bc38102c90ea2e98fda0b6dad64c5d500
Tsiory <tsioryrovantsoa@gmail.com>
2025-07-18 00:14:09 +0200

    festival mode

--- resources/views/festival/index.blade.php
+++ resources/views/festival/index.blade.php
@@ -6,20 +6,19 @@
     <div class="row">
         @foreach ($festivals as $festival)
             <div class="col-lg-4 col-md-6 mb-4">
-                <div class="card h-100 shadow-sm">
-                    <img src="https://picsum.photos/seed/festival{{ $festival->id }}/600/400" class="card-img-top"
-                        alt="Image Festival">
-
-                    <div class="discography__item__text">
-                        <span>{{ $festival->name }}</span>
-                        <p class="card-text">
-                            {{ $festival->description }}
+                <div class="event__item">
+                    <div class="event__item__pic set-bg"
+                        data-setbg="https://picsum.photos/seed/festival{{ $festival->id }}/600/400">
+                        <div class="tag-date">
+                            <span>{{ \Carbon\Carbon::parse($festival->startDate)->format('d M Y') }}</span>
+                        </div>
+                    </div>
+                    <div class="event__item__text">
+                        <h4>{{ $festival->name }}</h4>
+                        <p>
+                            <i class="fa fa-map-marker"></i> {{ $festival->location }},
+                            {{ $festival->region }}
                         </p>
-                        <p class="mb-1"><strong>Dates:</strong>
-                            {{ \Carbon\Carbon::parse($festival->startDate)->format('d M Y') }} -
-                            {{ \Carbon\Carbon::parse($festival->endDate)->format('d M Y') }}</p>
-                        <p class="mb-1"><strong>Lieu:</strong> {{ $festival->location }}, {{ $festival->city }},
-                            {{ $festival->region }}</p>
                     </div>
                 </div>
             </div>
